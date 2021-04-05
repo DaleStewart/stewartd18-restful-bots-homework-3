2c778a6755ac21f4c1984d09aa524a3dc8765771
DaleStewart <97stewart@gmail.com>
2021-04-05 13:30:38 -0400

    Added junit test for courseService No test implemented

---
+++ src/test/java/edu/ecu/cs/seng6285/restfulbots/datastore/CourseServiceTest.java
@@ -0,0 +1,19 @@
+package edu.ecu.cs.seng6285.restfulbots.datastore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CourseServiceTest {
+
+	@Test
+	public void testCreateCourse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllCourses() {
+		fail("Not yet implemented");
+	}
+
+}
